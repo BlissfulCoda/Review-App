8fbe81ed47efec2e2cfb4388c0a9e9eb579ddbf7
Ronnie Kiyegga <ronniekiyegga@hotmail.com>
2022-08-12 17:27:47 +0100

    feat: added Data Component for the data  and FeedbackItem.jsx file

--- review-app/src/App.jsx
+++ review-app/src/App.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import Header from "./Components/Header";
+import FeedbackData from "./Data/FeedbackData";
+import FeedbackItem from "./Components/FeedbackItems";
+
 export default function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <div>
       <Header title="Review App" />
+      <FeedbackItem />
     </div>
   );
 }
