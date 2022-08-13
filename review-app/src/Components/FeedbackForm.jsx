59b4a50fbd47b9f2a439ff26b08c7e6716fee074
Ronnie Kiyegga <ronniekiyegga@hotmail.com>
2022-08-13 14:04:47 +0100

    feat: add RatingStats.jsx component to handle selected rating

--- review-app/src/Components/FeedbackForm.jsx
+++ review-app/src/Components/FeedbackForm.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import Card from "./Shared/Card";
 import Button from "./Shared/Button";
+import RatingStats from "./RatingStats";
 
 export default function FeeddbackForm() {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
   const [isDisabled, setIsDisabled] = useState(true);
   const [message, setMessage] = useState("");
 
@@ -22,11 +24,11 @@ export default function FeeddbackForm() {
     setText(e.currentTarget.value);
   };
 
-  
   return (
     <Card className="input">
       <form>
         <h2>What would you rate this service ?</h2>
+        <RatingStats select={(rating) => setRating(rating)} />
         <section className="input-group">
           <input
             onChange={handleText}
